579c1d4eed1db5d97d30cf0552dfcff89940e01f
Marouane Hajji Naim <134142400+MarouaneHajji@users.noreply.github.com>
2025-01-05 10:42:10 -0800

    Update  1337script.py "AGAIN" ...

--- 1337script.py
+++ 1337script.py
@@ -1,5 +1,5 @@
-import tweepy
-import time
+import tweepy 
+import time 
 from plyer import notification
 
 API_KEY = 'LGlGh3ORzXADKBrI02MsKC6Tz'
@@ -7,27 +7,30 @@ API_SECRET_KEY = 'GgsVmaRVyBoAlaZGWaKosLZbmRGFDunXdHxxsfUlqIVjHYsPqa'
 ACCESS_TOKEN = '1874529763806543872-BDYehAjLumQd68bzxeZtVXCRbDIBkG'
 ACCESS_TOKEN_SECRET = 'HHpx37aoTDf2jbL9Wp9GhM51S3BzQ2LoRTuC244XRKLOX'
 
-auth = tweepy.OAuthHandler(API_KEY, API_SECRET_KEY)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-api = tweepy.API(auth)
+authentification = tweepy.OAuthHandler(API_KEY, API_SECRET_KEY)
+authentification.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(authentification)
 
-TWITTER_USERNAME = '1337FIL' 
-KEYWORDS = ['check-in', 'Check-In', 'Check-IN', 'CHECK-IN']
+twitter_username = '1337FIL'
+keywords_to_search = ['check-in', 'Check-In', 'Check-IN', 'CHECK-IN']
 
 def check_for_tweets():
     try:
-        tweets = api.user_timeline(screen_name=TWITTER_USERNAME, count=5, tweet_mode='extended')
+        tweets = api.user_timeline(screen_name=twitter_username, count=5, tweet_mode='extended')
+        
         for tweet in tweets:
-            if any(keyword.lower() in tweet.full_text.lower() for keyword in KEYWORDS):
+            if any(keyword.lower() in tweet.full_text.lower() for keyword in keywords_to_search):
                 print(f"Found tweet: {tweet.full_text}")
-                notify_user(tweet.full_text)
+                send_notification(tweet.full_text)
                 return
-        print("No new check-in tweets found.")
-    except Exception as e:
-        print(f"Error: {e}")
 
-def notify_user(message):
-    print("Sending notification...")
+        print("No new check-in tweets found.")
+    
+    except Exception as error:
+        print(f"Error: {error}")
+
+def send_notification(message):
+    print("Sending notification _ _ _")
     notification.notify(
         title="1337 Check-In Alert",
         message=message,
@@ -35,8 +38,9 @@ def notify_user(message):
     )
 
 try:
+    print("Starting the script. Press Ctrl+C to stop :)")
     while True:
-        check_for_tweets()
-        time.sleep(300) 
+        check_for_tweets() 
+        time.sleep(300)   
 except KeyboardInterrupt:
-    print("Exiting script.")
+    print("Script stopped by the user :(")
